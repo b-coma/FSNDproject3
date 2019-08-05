449de13655a29515e95162884ec254ad659811b1
b-coma <alicia.sarbu@gmail.com>
2019-08-05 16:40:11 -0400

    Add files via upload
    Establishing baseline for FSND project 3

---
+++ catalog_db_setup.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import datetime
+from sqlalchemy import Column, Integer, DateTime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'id': self.id,
+           'email':   self.email,
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    date_added = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'id': self.id,
+           'description': self.description,
+           'date_added': self.date_added,
+        }
+
+
+engine = create_engine('sqlite:///catalog3.db')
+
+
+Base.metadata.create_all(engine)
---
+++ lotsofitems.py
@@ -0,0 +1,108 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from catalog_db_setup import Base, User, Category, Item
+
+engine = create_engine('sqlite:///catalog3.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# delete from tables to initialize
+num_rows_deleted = session.query(Category).delete()
+session.commit()
+
+num_rows_deleted = session.query(Item).delete()
+session.commit()
+
+
+golfCat = Category(name="Golf")
+session.add(golfCat)
+session.commit()
+
+baseballCat = Category(name="Baseball")
+session.add(baseballCat)
+session.commit()
+
+tennisCat = Category(name="Tennis")
+session.add(tennisCat)
+session.commit()
+
+skiCat = Category(name="Downhill Skiing")
+session.add(skiCat)
+session.commit()
+
+item1 = Item(name="Putter", description="A golf club used for \
+    short shots on the green", category=golfCat)
+session.add(item1)
+session.commit()
+
+item2 = Item(name="Pitching Wedge", description="A golf club used \
+    for short shots that require loft", category=golfCat)
+session.add(item2)
+session.commit()
+
+item3 = Item(
+    name="Nine Iron",
+    description="greatest loft and the shortest shaft",
+    category=golfCat)
+session.add(item3)
+session.commit()
+
+item4 = Item(
+    name="Eight Iron",
+    description="A golf club with a small head and an angled face. \
+    As the number of the iron increases, the loft increases and the \
+    length of the shaft decreases, making the eight iron intermediate",
+    category=golfCat)
+session.add(item4)
+session.commit()
+
+item5 = Item(
+    name="Seven Iron",
+    description="A golf club with a small head and an angled face.  \
+    As the number of the iron increases, the loft increases and the \
+    length of the shaft decreases, making the seven iron intermediate",
+    category=golfCat)
+session.add(item5)
+session.commit()
+
+item6 = Item(
+    name="Six Iron",
+    description="A golf club with a small head and an angled face.  \
+    As the number of the iron increases, the loft increases and the \
+    length of the shaft decreases, making the six iron intermediate",
+    category=golfCat)
+session.add(item6)
+session.commit()
+
+item7 = Item(
+    name="Five Iron",
+    description="A golf club with a small head and an angled face.  \
+    As the number of the iron increases, the loft increases and the \
+    length of the shaft decreases, making the five iron intermediate",
+    category=golfCat)
+session.add(item7)
+session.commit()
+
+item8 = Item(
+    name="Four Iron",
+    description="A golf club with a small head and an angled face.  \
+    As the number of the iron increases, the loft increases and the \
+    length of the shaft decreases, making the four iron intermediate",
+    category=golfCat)
+session.add(item8)
+session.commit()
+
+item9 = Item(
+    name="Three Iron",
+    description="A golf club with a small head and an angled face.  \
+    The three iron has the smallest loft and the longest shaft relative \
+    to other irons",
+    category=golfCat)
+session.add(item8)
+session.commit()
+
+print "A few items added, categories Golf, Baseball, Tennis, Downhill Skiing"
